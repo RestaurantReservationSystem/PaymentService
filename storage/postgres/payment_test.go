1ed25eccd85c2f3c4ed21fcbabcbac98ecb0b8d4
HatamovJamshidbek <hatamovjamshid47@gmail.com>
2024-07-08 21:15:03 +0500

    commit

--- storage/postgres/payment_test.go
+++ storage/postgres/payment_test.go
@@ -2,35 +2,11 @@ package postgres
 
 import (
 	"fmt"
-	"log"
-	"log/slog"
 	pb "payment_service/genproto"
 	"reflect"
 	"testing"
 )
 
-//	func TestConnection(t *testing.T) {
-//		db, err := Connection()
-//		if err != nil {
-//			panic(err)
-//		}
-//
-// }
-func TestPaymentRepository_CreatePayment(t *testing.T) {
-	db, err := Connection()
-	if err != nil {
-		log.Fatal("error is connection db")
-	}
-	repo := NewPaymentRepository(db)
-	request := pb.CreatePaymentRequest{PaymentStatus: "pending", PaymentMethod: "card", Amount: 4.5, ReservationId: "1234alk;sdl;kf"}
-	response, err := repo.CreatePayment(&request)
-	if err != nil {
-		fmt.Println("+++++++", err)
-		//log.Fatalf("error internal server error ->", err.Error())
-	}
-	expected := &pb.Void{}
-	if reflect.DeepEqual(response, expected) {
-		slog.StringValue("message is success")
 func TestPaymentRepository_CreatePayment(t *testing.T) {
 	db, err := Connection()
 	if err != nil {
@@ -55,7 +31,6 @@ func TestPaymentRepository_CreatePayment(t *testing.T) {
 		t.Errorf("Response does not match expected value.\nGot: %+v\nExpected: %+v", response, &pb.ReservationResponse{})
 
 	}
-
 }
 func TestPaymentRepository_DeletePayment(t *testing.T) {
 	db, err := Connection()
@@ -134,7 +109,7 @@ func TestPaymentRepository_GetPayment(t *testing.T) {
 	}
 }
 
-func TestAllPaymentRepository_GetPayment(t *testing.T) {
+func TestAllPaymentRepository_GetAllPayment(t *testing.T) {
 	db, err := Connection()
 	if err != nil {
 
